ebebe742d01fbe86d5ee4e6ecd7aaae6e77196da
BonmooKoo <rnqhsan9812@gmail.com>
2025-06-13 15:17:14 +0900

    TODO:fix code

--- rdma_coroutine.hpp
+++ rdma_coroutine.hpp
@@ -1,96 +1,23 @@
-#pragma once
 #include "rdma_verb.h"
-#include <boost/coroutine2/symmetric_coroutine.hpp>
+#include "rdma_common.h"
+#include <boost/coroutine/symmetric_coroutine.hpp>
 #include <vector>
 #include <functional>
 #include <atomic>
 #include <infiniband/verbs.h>
 
-// 1) Boost.Coroutine2 타입 정의
-using coro_t    = boost::coroutines2::symmetric_coroutine<void>;
-using CoroCall  = coro_t::call_type;
-using CoroYield = coro_t::yield_type;
 
-thread_local CoroCall master;
-thread_local int thread_id
-//thread 별로 다른 key 갖음
-int* key;
-// 전역 카운터들
-static uint64_t g_total_ops = 0;
-static int g_coro_cnt;
-//for debug
-static std::atomic<uint64_t> g_ops_started{0};
-//count how many 
-static std::atomic<uint64_t> g_ops_finished{0};
-
-static int get_key(){
-   uint64_t idx = g_ops_started.fetch_add(1, std::memory_order_relaxed);
-   idx=idx%g_total_ops;//g_total_ops = TOTALOP = 32M
-   return key[idx];
-}
-// 2) Worker 코루틴 본체
-static void coro_worker(CoroYield &yield,
+int get_key();
+void coro_worker(CoroYield &yield,
                         RequestGen gen,
-                        int coro_id)
-{
-  int key;
-  while(g_ops_started < g_total_ops){
-   //1) get key
-    key=get_key();
-    // 2) RDMA post (pseudo code)
-    rdma_read_nopoll((key % (ALLOCSIZE / SIZEOFNODE)) * SIZEOFNODE,8, 0,thread_id,coro_id);
+                        int coro_id);
 
-    // 3) 완료 대기: master 로 제어권 넘기기
-    yield();
-
-    // (resume 후) 후처리
-    ++g_ops_finished;
-  }
-
-  // 루프 종료 후에도 master에게 복귀
-  yield();
-}
-
-// 3) Master 코루틴 본체
-static void coro_master(CoroYield &yield,
+ void coro_master(CoroYield &yield,
                         int coro_cnt,
-                        std::vector<CoroCall> &worker)
-{
-  // 3-1) 초기 실행: 각 워커를 한 번 깨워서 시작하게 만듦
-  for (int i = 0; i < coro_cnt; ++i) {
-    yield(worker[i]);
-  }
-
-  // 3-2) 이벤트 루프: CQ 폴링l
-  while (g_ops_finished < g_total_ops) {
-    int next_id=poll_coroutine(thread_id);
-    yield(worker[next_id]);
-  }
-}
-// run_coroutine(int thread_id,int coro_cnt, int* key[],int threadcount)
-// 4) run_coroutine 함수: Tree::run_coroutine 과 동일한 형태
-static void run_coroutine(int thread_id,
+                        std::vector<CoroCall> &worker);
+void run_coroutine(int thread_id,
                           int coro_cnt,
                           int* key_arr,
                           int threadcount,
                           int total_ops
-                        )
-{
-//0.key
-  key=key_arr;
-  g_total_ops=total_ops;
-  //1. coroutine vector 생성
-  std::vector<CoroCall> worker(coro_cnt);
-
-  //2. Client 생성
-  for (int i = 0; i < coro_cnt; ++i) {
-    worker[i] = CoroCall(std::bind(&coro_worker,/*yield*/ _1,/*coro_id*/ i));
-  }
-
-  // 4-2) 마스터 코루틴 생성
-  master = CoroCall(
-    std::bind(&coro_master, _1, coro_cnt, std::ref(worker))
-  );
-  // 4-3) 최초 진입
-  master();
-}
+                        );
